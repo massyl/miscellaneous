0fcbc71beaf72a974a24866b3a3aa08de74aa4a6
massyl <massil.nait@gmail.com>
2014-10-04 19:49:27 +0200

    some code refactoring

--- State.hs
+++ State.hs
@@ -1,20 +1,35 @@
 {-# LANGUAGE RankNTypes #-}
-import Control.Monad.Trans.Cont
+module StateCont (
+   -- | inject pure value inside a state full computation
+   inject, 
+   -- |Chain statfull computations
+   (>>>=), 
+   -- | Consult the state
+   get,
+   -- Change the state
+   put,
+   -- | modify the state using the given function
+    modify, 
+   -- | applies the given function to result curried by one statefull computation
+   mapS, 
+   -- | State type constructor
+   State)
+where
 
 newtype State s a = State { runState :: forall r. s -> (a -> s -> r) -> r }
 
 -- | alternative implementation of return
--- | return' a = State $ \s f -> f a s
-return' :: a -> State s a
-return' a = State $ \s -> flip ($ a) s
+-- | inject a = State $ \s f -> f a s
+inject :: a -> State s a
+inject a = State $ flip ($ a) 
 
 mapS :: (a -> b) -> State s a -> State s b
-mapS f = (>>>= return' . f)
+mapS f = (>>>= inject . f)
 
 bind :: (a -> State s b) -> State s a -> State s b
 f `bind` st =  State $ \s0 -> runState st s0 $ \a s' k -> runState (f a) s' k
-infixr 1 >>>=
 
+infixr 1 >>>=
 (>>>=) :: State s a -> (a -> State s b) -> State s b
 st >>>= f = f `bind` st
 
@@ -28,16 +43,16 @@ modify  :: (s -> s) -> State s ()
 modify f = State $ \s k -> k () $ f s
 
 execState :: s -> State s a -> s
-execState s st = runState st s $ \_ s' -> s'
+execState s st = runState st s $ flip const
 
 evalState :: s -> State s a -> a
-evalState s st = runState st s $ \a _ -> a
+evalState s st = runState st s const
 
 
 
 -- | making State S a an instance fo Monad to make do notation availalbe
 instance Monad (State s) where
- return = return'
+ return = inject
  (>>=) = (>>>=)
 
 -- | simple function that manipulate the state, just to show how we can use it
@@ -46,13 +61,13 @@ useState = do
           s <- get
           let r = s*3 + 1
           put r
-          modify (\x -> x * 3)
+          modify (* 3)
           return r
 
 -- | Run computations using State S a
 main = do
-      putStrLn $ show $ (runState $ return' 1) 3 (+)
-      print $ (runState $ get >>>= (\a -> return' $ a + 2)) 0 (+)
+      print $ (runState $ inject 1) 3 (+)
+      print $ (runState $ get >>>= (\a -> inject $ a + 2)) 0 (+)
       putStrLn "using execState with get and put : "
       print $  execState 1 useState 
       putStrLn "using evalState with get and put : "
