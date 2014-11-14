035df68a44de74c871758a68233206e888942e1e
massyl <massil.nait@gmail.com>
2014-11-15 00:00:08 +0100

    some code reoganization

--- Yoneda.hs
+++ Yoneda.hs
@@ -7,8 +7,23 @@
 module Yoneda where
 import Data.Functor.Identity
 
+------------------------------------------------------------------------------------
+-- | Let's abstact over a Functor and write generic version of check and uncheck
+-----------------------------------------------------------------------------------
+newtype Yoneda f a = Yoneda {runYoneda :: forall b. (a -> b) -> f b }
+
+machine :: Functor f => Yoneda f a
+machine =  Yoneda $ \f -> fmap f a  where a = undefined
+
+uncheck :: Yoneda f a -> f a
+uncheck f = runYoneda f id
+
+check :: Functor f =>  f a -> Yoneda f a
+check fa = Yoneda $ \f -> fmap f fa
+
 --------------------------------------------------------------
 -- | Let's define a machine whith the following signature
+-- | forall b. (a -> b) -> b
 --------------------------------------------------------------
 type IdentityYoneda a = Yoneda Identity a
 
@@ -29,8 +44,8 @@ check1  a  = Yoneda $ \f -> Identity $ f a
 
 --------------------------------------------------------------
 -- | let's define a new machine that holds a list of a : [a]
+-- | forall b. (a -> b) -> [b]
 --------------------------------------------------------------
--- type Yoneda2 a = (forall b. (a -> b) -> [b])
 type ListYoneda a = Yoneda [] a
 
 machine2 :: ListYoneda a
@@ -43,13 +58,13 @@ uncheck2 :: ListYoneda a -> [a]
 uncheck2 f = runYoneda f id
 
 --------------------------------------------------------------
--- | Let's construct a new machine out of a given : a
+-- | Let's construct a new machine out of a given : as
 --------------------------------------------------------------
 check2 :: [a] -> ListYoneda a
 check2 as  = Yoneda $ \f ->  fmap  f as 
 
 --------------------------------------------------------------
--- | let's define a new machine that holds a list of a : [a]
+-- | let's define a new machine that holds a function c -> a
 --------------------------------------------------------------
 type ArrowYoneda a c = Yoneda ((->) c) a
 
@@ -61,20 +76,4 @@ uncheck3 f = runYoneda f id
 
 check3 :: (c -> a) -> ArrowYoneda a c
 check3 f = Yoneda $ \g -> g . f
-
-------------------------------------------------------------------------------------
--- | Let's abstact over a Functor and write generic version of check and uncheck
------------------------------------------------------------------------------------
-newtype Yoneda f a = Yoneda {runYoneda :: forall b. (a -> b) -> f b }
-
-machine :: Functor f => Yoneda f a
-machine =  Yoneda $ \f -> fmap f a 
-              where a = undefined
-
-uncheck :: Yoneda f a -> f a
-uncheck f = runYoneda f id
-
-check :: Functor f =>  f a -> Yoneda f a
-check fa = Yoneda $ \f -> fmap f fa
-
 
